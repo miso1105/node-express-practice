d471d0ac1d99c9b0ae3a3451734eedabab1d88dc
Miso <un5754@naver.com>
2025-03-06 04:36:23 +0900

    Feat: Post CRUD
    - controllers/page.js: 전체 게시글 조회, 게시글 작성 화면,작성한 post 조회, 게시글 아이디로 조회 추가
    - controllers/post.js: 게시글 생성, 수정, 삭제 컨트롤러
    - routes/page.js: 게시글 관련 화면 라우터 추가가
    - routes/post.js: 게시글 생성, 수정, 삭제 라우터
    - views/layout.html: 로그인 상태에 따른 화면 구성 변경
    - views/main.html: 메인 페이지에 모든 게시글 렌더링
    - views/myposts.html: 자신이 작성한 게시글 렌더링
    - views/post.html: 게시글 작성 폼
    - views/search.html: 로그인 없이 게시글 아이디 조회, 본인이 작성한 게시글이라면 수정/ 삭제 가능, axios를 활용한 비동기 HTTP 요청

--- routes/page.js
+++ routes/page.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { renderMain, renderJoin } = require('../controllers/page');
-const { isNotLoggedIn } = require('../middlwares');
+const { renderMain, renderJoin, renderPost, renderMyPosts, renderPostByPostId } = require('../controllers/page');
+const { isLoggedIn, isNotLoggedIn } = require('../middlwares');
 
-// router.use((req, res, next) => {
-//     res.locals.user = req.user; // passport 과정 이후
+// router.use((req, res, next) => { // app.use로 앞-뒤 다 사용 가능
+//     res.locals.user = req.user; // passport 과정 이후 라우터끼리만(뒤) user 사용 가능, 템플릿 not in use
 //     next(); 
 // });
 
 router.get('/', renderMain); // 메인 화면 
 router.get('/join', isNotLoggedIn, renderJoin); // 라우터에서 컨트롤러 호출
+router.get('/post', isLoggedIn, renderPost);
+router.get('/myposts', isLoggedIn, renderMyPosts); // 유저 id로 게시글들 가져오기 / api용으로 만들거면 GET 메서드 + getPostsById 컨트롤러로 res.status(200).json(results); 반환하기
+router.get('/search', renderPostByPostId);
 
 module.exports = router;
